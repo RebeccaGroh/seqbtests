b18703df1bb388403f1f29f1e0485a6a352ff198
Groh <Rebecca.Groh@campus.lmu.de>
2019-07-10 15:16:44 +0200

    check structure

--- tests/testthat/test-check_structure.R
+++ tests/testthat/test-check_structure.R
@@ -1,3 +1,68 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("check_structure")
+
+# check if check_names() works correctly for test_benchmark 
+test_that("check_names for test_benchmark", {
+  expect_true(check_names(data = test_benchmark, problemset = "problem_a", 
+                          baseline = "algo_1", learner_b = "algo_2", 
+                          measure = "measure_col"))
 })
+
+
+# check if check_column_names() works correctly for test_benchmark 
+test_that("check_column_names for test_benchmark", {
+  expect_true(check_column_names(df = test_benchmark))
+})
+
+
+# check if check_structure() works correctly for test_benchmark
+test_that("check_structure for test_benchmark", {
+  expect_true(check_structure(test_benchmark))
+})
+
+
+# check if check_structure() throws error without problem, algorithm 
+# and replications
+test_that("check_structure without problem/algorithm/replications", {
+  tmp <- test_benchmark
+  tmp$problem <- NULL 
+  expect_error(check_structure(tmp))
+  tmp <- test_benchmark
+  tmp$algorithm <- NULL 
+  expect_error(check_structure(tmp))
+  tmp <- test_benchmark
+  tmp$replications <- NULL 
+  expect_error(check_structure(tmp))
+})
+
+
+# check if check_structure() throws error for unknown column
+test_that("check_structure with unknown column", {
+  tmp <- test_benchmark
+  tmp$unknown <- rep(1, nrow(tmp))
+  expect_error(check_structure(tmp))
+})
+
+# check if check_structure() throws error without measure column
+test_that("check_structure without problem/algorithm/replications", {
+  tmp <- test_benchmark
+  tmp$measure_col <- NULL 
+  expect_error(check_structure(tmp))
+})
+
+
+# check if check_structure() throws error when NAs exist
+test_that("check_structure with NAs", {
+  tmp <- test_benchmark
+  tmp$algorithm[[1]] <- NA
+  expect_error(check_structure(tmp))
+  tmp <- test_benchmark
+  tmp$parameter_algorithm[[1]] <- NA
+  expect_error(check_structure(tmp))
+  tmp <- test_benchmark
+  tmp$measure_col[[1]] <- NA
+  expect_error(check_structure(tmp))
+  tmp <- test_benchmark
+  tmp$measure_col[[1]] <- "error"
+  expect_error(check_structure(tmp))
+})
+
