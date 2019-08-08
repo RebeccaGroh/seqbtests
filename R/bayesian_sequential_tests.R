51e6c067f29e8fa31b61db2c118391268cf8fb03
Groh <Rebecca.Groh@campus.lmu.de>
2019-08-08 15:35:12 +0200

    test for equal or better

--- R/bayesian_sequential_tests.R
+++ R/bayesian_sequential_tests.R
@@ -10,8 +10,9 @@
 #' @param learner_b Second algorithm. If not defined, every algorithm will be 
 #' tested against baseline. 
 #' @param measure Measure column. 
-#' @param test Defines whether the performances should be tested for either 
-#' being better ('better') or being just as good ('equal').
+#' @param compare Defines whether the performances should be tested for either 
+#' being better ('better') or being just as good ('equal'). If not defined, the 
+#' default is to test for 'better'.
 #' @param rho Correlation factor. 
 #' @param rope Region of practical equivalence. 
 #' @param max_repls maximum number of replications that should be build in 
@@ -29,15 +30,22 @@
 #' Note that the default value for measure is the first measure column in the 
 #' data frame. The default of rho is 0.1. If rho equals 0 this converts the test 
 #' in the equivalent of the standard t test. 
-#' @example results <- seq_b_corr_t_test(test = 'equal', df = test_benchmark_small, problemset = 'problem_a', baseline = 'algo_1', max_repls = 10, rho=0.1, rope=c(-0.01, 0.01))
+#' @example 
+#' results <- seq_b_corr_t_test(test = 'equal', df = test_benchmark_small, 
+#'                              problemset = 'problem_a', baseline = 'algo_1', 
+#'                              max_repls = 10, rho=0.1, rope=c(-0.01, 0.01))
 #' @export
-seq_b_corr_t_test <- function(problemset, baseline, learner_b = NULL, measure = NULL, test = NULL, rho = 0.1, rope = c(-0.01, 0.01), max_repls = 20, ...) {
+seq_b_corr_t_test <- function(problemset, baseline, learner_b = NULL, 
+                              measure = NULL, compare = NULL, rho = 0.1, 
+                              rope = c(-0.01, 0.01), max_repls = 20, ...) {
   result = data.frame()
   for (i in 2:max_repls) {
     data <- get_replications(i, ...)
     ## check if passed names, define columns in dataset
     checkmate::assert_true(check_structure(df = data))
-    checkmate::assert_true(check_names(df = data, problemset, baseline, learner_b = NULL, measure = NULL, parameter_algorithm = NULL))
+    checkmate::assert_true(check_names(df = data, problemset, baseline, 
+                                       learner_b = NULL, measure = NULL, 
+                                       parameter_algorithm = NULL))
     if (is.null(measure)) {
       measure <- get_measure_columns(data)[1]
     }
@@ -51,9 +59,11 @@ seq_b_corr_t_test <- function(problemset, baseline, learner_b = NULL, measure = 
     for (k in algorithms[algorithms != baseline]) {
       if (!is.null(learner_b)) {
         k <- learner_b
-        y <- data[data[["problem"]] == problemset & data[["algorithm"]] == k, measure]
+        y <- data[data[["problem"]] == problemset 
+                  & data[["algorithm"]] == k, measure]
       } else {
-        y <- data[data[["problem"]] == problemset & data[["algorithm"]] == k, measure]
+        y <- data[data[["problem"]] == problemset 
+                  & data[["algorithm"]] == k, measure]
       }
       # Bayesian correlated t Test
       b_test <- scmamp::bCorrelatedTtest(x, y, rho, rope)
@@ -67,7 +77,15 @@ seq_b_corr_t_test <- function(problemset, baseline, learner_b = NULL, measure = 
       result[k, "rope"] <- b_test$posterior.probabilities[2]
       result[k, "right"] <- b_test$posterior.probabilities[3]
       result[k, "repls"] <- i
-      if (b_test$posterior.probabilities[3] > 0.95) {
+      if (compare == "better") { 
+        threshold <- b_test$posterior.probabilities[3]
+      } else if (compare == "equal") {
+        threshold <- b_test$posterior.probabilities[2] + 
+          b_test$posterior.probabilities[3]
+      } else {
+        threshold <- b_test$posterior.probabilities[3]
+      }
+      if (threshold > 0.95) {
         result[k, "significance_appears"] <- TRUE
       } else {
         result[k, "significance_appears"] <- FALSE
@@ -77,11 +95,3 @@ seq_b_corr_t_test <- function(problemset, baseline, learner_b = NULL, measure = 
   }
   return(result)
 }
-
-#------------------------------------------------------------------------------
-# erst später einbauen, wenn das richtige Ergebnis angezeigt wird if (is.null(test)) { ## test for better threshold
-# <- b_test$posterior.probabilities[3] } else if (test == 'equal') { threshold <- b_test$posterior.probabilities[2] +
-# b_test$posterior.probabilities[3] } else { threshold <- b_test$posterior.probabilities[3] }
-
-
-
