9f8fcf2b9093dbebd089d5d3a6024a3ba68638f7
RebeccaGroh <rebeccagroh@hotmail.de>
2019-09-16 17:55:13 +0200

    data_set_up

--- attic/data_set_up.R
+++ attic/data_set_up.R
@@ -1,34 +1,30 @@
 
+result$lrn.cl <- str_sub(result$lrn.cl, start=9L, end=30L)
+unique(result$lrn.cl)
+
+result$algorithm <- paste(result$lrn.cl, result$feat.extract.method, sep = "_")
+result$algorithm <- paste(result$algorithm, result$tune, sep = "_")
+
 # drop time_queued, time_running, n, ntrain, ntest, length, nclasses, minorityclass_size,
 # ber, timeboth 
 result_small <- subset(result, tune == "tune", 
                        select= -c(time.queued, time.running, n, ntrain, ntest, 
                                   length, nclasses, feat.extract.method, 
                                   minorityclass_size, algo.type,
-                                  ber, timeboth, job.id, tune))
+                                  ber, timeboth, job.id, tune, lrn.cl, algo.pars))
 
 ## check for duplicates (drops rows that are duplicated)
 result_small <- unique(result_small)
 
-View(result_small)
+# rename values and measure column value von ranger.pow vorher umbenennen 
+colnames(result_small)[colnames(result_small) == "mmce"] <- "measure_mmce"
+colnames(result_small)[colnames(result_small) == "repls"] <- "replications"
 
-result_small$algo.pars[44]
 
 # knn_dtw_tuned | ranger_none_tuned | ranger_wavelet_tuned
 # lrn.cl_feat.extract.method_tune? 
 
-# kann man vorher einfach die notwendigen Cols zusammen pasten und dann 
-# alles andere löschen? dann muss man nicht auf die Liste in algo.pars zugreifen 
+# kann mit dem Datensatz jetzt gearbeitet werden? durchprobieren, welcher 
+# Algorithmus als Baseline verwendet werden soll 
 
-# relevante Variablen: lrn.cl, ft.extract.method, tune.ctrl.method
-
-unique(result$tune)
-unique(result$feat.extract.method)
-unique(result$lrn.cl)
-
-# drop none_tuned learners
-
-require(stringr)
-class(result$lrn.cl)
-result$lrn.cl <- str_sub(result$lrn.cl, start=8L, end=30L)
-unique(result$lrn.cl)
+## Datensatz fertig aufbereitet 
